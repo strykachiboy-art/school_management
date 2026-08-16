64999771f60622972c5a3a9fc8fa6e23572de022
Stryka-Max <strykachiboy@gmail.com>
2026-08-16 13:22:52 +0200

    initial commit

--- App/models/term.py
+++ App/models/term.py
@@ -6,13 +6,14 @@ def _utcnow():
     return datetime.now(timezone.utc)
 
 class Term(db.Model):
-    
+    __tablename__ = "terms"
+
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(30), nullable = False)
     end_date = db.Column(db.Date, nullable = False)
     is_current = db.Column(db.Boolean, default = False, nullable = False)
-    academic_session_id = db.Column(db.Integer, db.ForeignKey("academic_session.id"))
+    academic_session_id = db.Column(db.Integer, db.ForeignKey("academic_sessions.id"))
     created_at = db.Column(db.DateTime, default = _utcnow, nullable = False)
     updated_at = db.Column(db.DateTime, default = _utcnow, onupdate = _utcnow, nullable = False)
     
-    academic_session = db.relationship("AcademicSession", back_populates = "term")
+    academic_session = db.relationship("AcademicSession", back_populates="terms")
